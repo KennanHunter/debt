edccb848ac4a3ffe5589ac632529f8f5d154396b
Kennan Hunter <kennanhunter5@gmail.com>
2022-10-08 22:05:20 -0400

    debt view

--- app/src/router/Debt.tsx
+++ app/src/router/Debt.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mantine/core";
+import { ActionIcon, Box, Divider, Group, Paper, Title } from "@mantine/core";
+import { IconEdit } from "@tabler/icons";
 import { useLoaderData } from "react-router-dom";
 import { useDebt } from "../lib/hooks/useDebt";
 
@@ -17,7 +18,22 @@ export const DebtLoader = ({
 const Debt = ({}) => {
 	const id = useLoaderData() as ReturnType<typeof DebtLoader>;
 	const [debt] = useDebt(id);
-	return <Box>{debt?.reason}</Box>;
+	return (
+		<Box>
+			<Paper p="sm">
+				<div>
+					<Group position="apart">
+						<Title order={2}>{debt?.reason}</Title>
+						<ActionIcon>
+							<IconEdit />
+						</ActionIcon>
+					</Group>
+					<Divider />
+				</div>
+				<pre>{JSON.stringify(debt, null, 4)}</pre>
+			</Paper>
+		</Box>
+	);
 };
 
 export default Debt;
